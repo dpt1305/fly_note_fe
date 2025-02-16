3b1688a2de1f0bcd743aeb9c9f4527295ff1a01d
tungjj <tungplatin@gmail.com>
2025-02-16 14:35:50 +0700

    layout left side menu

--- src/components/left-menu.tsx
+++ src/components/left-menu.tsx
@@ -1,15 +1,49 @@
+import './left-menu.css';
+import { DynamicIcon } from 'lucide-react/dynamic';
+import commonStyle from '@/css/common.module.css';
+
+
 export default function LeftMenu() {
-    return (
-        <div className="container">
-            <div className="account-area"></div>
+	const notes = [
+		{
+			icon: 'icon',
+			title: 'title1',
+		},
+		{
+			icon: 'icon2',
+			title: 'title2',
+		}
+	]
+	return (
+		<div className="container-div">
+			{/* <div className="container-div">
+				<p>sdfasdf</p>
+				<p>asfd asdfasfasdf</p>
+			</div>
+			 */}
 
-            <div className="default-buttons">
-                <div className="icon-area"></div>
-                <div className="text-area"></div>
-            </div>
+			{/* <div className="account-area"></div>
 
-            <div className="notes-area"></div>
-            <p>LeftMenu</p>
-        </div>
-    )
+			{/* {
+						notes.map((note) => {
+							return (
+								<div className="notes-area">
+									<div className="icon-area"> <p>{note.icon}</p></div>
+									<div className="text-area"> <p>{note.title}</p></div>
+								</div>
+							)
+						})
+					} */}
+			<div className="default-buttons">
+				<div className="icon-area"> <DynamicIcon className={commonStyle.center} name="camera" color="red" size={14} /></div>
+				<div className="text-area"> <p>Text</p></div>
+			</div>
+			<div className="default-buttons">
+				<div className="icon-area"> <DynamicIcon className={commonStyle.center} name="alarm-clock" color="red" size={14} /></div>
+				<div className="text-area"> <p>Text</p></div>
+			</div>
+
+			<div className="notes-area"></div> 
+		</div>
+	)
 }
